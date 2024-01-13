166948b075830b25240653b51dd7c4b51f15fc2f
nastyaDushkina <anastasia.duschckina@yandex.ru>
2024-01-13 22:15:35 +0300

    create init migrations; create wrap function to launch up/down migrations

---
+++ sso/cmd/migrator/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	//библиотека для миграций
+	"github.com/golang-migrate/migrate/v4"
+	// драйвер для выполнения миграций sqlite3
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	// драйвер для получения миграций из файлов
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// go run ./cmd/migrator --storage-path=./storage/sso.db --migrations-path=./migrations
+
+// migrationsTable - задаём для возможности хранения версии тестовой базы (для тестов)
+// для тестов будут тестовые миграции
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storagePath, "storage-path", "", "path to database")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of igrations table")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	//создаём экземпляр мигратора
+	//x-migrations-table - отвечает за путь до таблицы версий миграций
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
---
+++ sso/migrations/1_init.up.sql
@@ -0,0 +1,15 @@
+-- Active: 1705091202434@@127.0.0.1@3306
+CREATE TABLE IF NOT EXISTS users
+(
+    id        INTEGER PRIMARY KEY,
+    email     TEXT NOT NULL UNIQUE,
+    pass_hash BLOB NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_email ON users (email);
+
+CREATE TABLE IF NOT EXISTS apps
+(
+    id     INTEGER PRIMARY KEY,
+    name   TEXT NOT NULL UNIQUE,
+    secret TEXT NOT NULL UNIQUE
+);
